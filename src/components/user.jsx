ba961311b69723b78139fd98d2f7e59d2e8179cb
ZagSerSan <yan.steys1997@gmail.com>
2023-08-26 19:18:12 +0200

    favorite func and btn

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,15 +3,16 @@ import PropTypes, { object } from 'prop-types';
 import Button from './button';
 import { useStudents } from '../hooks/useStudents';
 import { getBookmarkStatus } from '../fakeApi/students';
+import { Link } from 'react-router-dom';
 
 const User = ({ userData }) => {
   const bookmarkStatus = getBookmarkStatus(userData.id)
-  
+
   const { getFavorite } = useStudents()
   const handleClick = () => {
-    console.log(handleClick)
+    console.log('handleClick')
+    //! тут можно было бы сделать history.push('/userData.id')
   }
-
 
   return (
     <li className="w-[350px] h-[500px] border border-gray-300 rounded-md shadow-lg flex flex-col justify-between items-center p-4">
@@ -23,6 +24,7 @@ const User = ({ userData }) => {
       <div>О себе: {userData.info}</div>
       <div>
         <Button
+          type = 'open'
           color='bg-blue-500'
           onClick={handleClick}
           text='открыть'
