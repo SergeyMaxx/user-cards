10c4396f1ce0c99f2c042d18182cb845e85cc84b
vakitaki <vakitaki@yandex.com>
2023-08-26 21:09:06 +0300

    update

--- src/layouts/favorite.jsx
+++ src/layouts/favorite.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import { useStudents } from '../hooks/useStudents'
-import User from '../components/user'
+import React from "react";
+import { useStudents } from "../hooks/useStudents";
+import User from "../components/user";
 
 const Favorite = () => {
-  const { students, favorite } = useStudents()
+  const { students, favorite } = useStudents();
 
-  return (<>
-    <section className="flex flex-col justify-between gap-2 items-center my-20">
-      <h1 className=" uppercase text-3xl text-gray-900 ">
-        Избранное
-      </h1>
-      <div className=" w-1/3 h-1 bg-black bg-gradient-to-r from-cyan-500 to-blue-500" />
+  return (
+    <>
+      <section className="flex flex-col justify-between gap-2 items-center my-20">
+        <h1 className=" uppercase text-3xl text-gray-900 ">Избранное</h1>
+        <div className=" w-1/3 h-1 bg-black bg-gradient-to-r from-cyan-500 to-blue-500 mb-10" />
 
-      {localStorage.getItem('favorite')
-        ? (
+        {localStorage.getItem("favorite") ? (
           <div className="">
-            {students.length !== 0 && favorite && favorite.map(favId => (
-              <ul key={students[favId-1].id} className="flex flex-wrap gap-10">
-                <User userData={students[favId-1]} />
-              </ul>
-            ))}
-          </div>  
-        )
-        : <h1 className='mt-[20px]'>Избранных нету</h1>
-      }
-      
-    </section>
+            {students.length !== 0 &&
+              favorite &&
+              favorite.map((favId) => (
+                <ul
+                  key={students[favId - 1].id}
+                  className="flex flex-wrap gap-10 justify-center"
+                >
+                  <User userData={students[favId - 1]} />
+                </ul>
+              ))}
+          </div>
+        ) : (
+          <h1 className="mt-[20px]">Избранных нету</h1>
+        )}
+      </section>
+    </>
+  );
+};
 
-  </>
-  )
-}
-
-export default Favorite
+export default Favorite;
--- src/layouts/mainPage.jsx
+++ src/layouts/mainPage.jsx
@@ -1,29 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Outlet } from 'react-router-dom';
-import User from '../components/user';
-import api from '../fakeApi';
+import React from "react";
+import { useParams, Outlet } from "react-router-dom";
+import User from "../components/user";
+import { useStudents } from "../hooks/useStudents";
 
 function MainPage() {
   const { id } = useParams();
-  const [students, setStudents] = useState(null);
-
-  useEffect(() => {
-    api.students().then(data => setStudents(data));
-  }, []);
+  const { students } = useStudents();
 
   return id ? (
     <Outlet />
   ) : (
     <section className="flex flex-col justify-between gap-2 items-center my-20">
-      <h1 className=" uppercase text-3xl text-gray-900 ">
-        Наша команда
-      </h1>
+      <h1 className=" uppercase text-3xl text-gray-900 ">Наша команда</h1>
       <div className="w-1/3 h-1 bg-black bg-gradient-to-r from-cyan-500 to-blue-500 mb-10" />
-      {students &&
-        <ul className="flex flex-wrap gap-10">
-          {students.map(u => <User key={u.id} userData={u} />)}
+      {students && (
+        <ul className="flex flex-wrap gap-10 justify-center">
+          {students.map((u) => (
+            <User key={u.id} userData={u} />
+          ))}
         </ul>
-      }
+      )}
     </section>
   );
 }
