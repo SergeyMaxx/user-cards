8641098915bc5f1a7e595e53e7493e893a02dfbe
vakitaki <vakitaki@yandex.com>
2023-08-26 17:18:07 +0300

    update

--- src/layouts/mainPage.jsx
+++ src/layouts/mainPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import MainSlider from "../components/mainSlider";
 import { useParams, Outlet } from "react-router-dom";
-import ProgressBar from "../components/progressBar";
 
 function MainPage() {
   const { id } = useParams();
@@ -9,7 +7,7 @@ function MainPage() {
     <Outlet />
   ) : (
     <section className="flex flex-col justify-between gap-2 items-center my-20">
-      <h1 className=" uppercase text-3xl text-gray-900 ">Наша команда</h1>
+      <h1 className=" uppercase text-3xl text-red-900 ">Наша команда</h1>
       <div className=" w-1/3 h-1 bg-black bg-gradient-to-r from-cyan-500 to-blue-500"></div>
     </section>
   );
