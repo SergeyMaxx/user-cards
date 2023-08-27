774b2f04ec499ee45f2521dd651c8ef2480d3986
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 17:26:30 +0200

    upd styles

--- src/components/comments/comment.jsx
+++ src/components/comments/comment.jsx
@@ -7,7 +7,7 @@ function Comment({ comment }) {
 
   return (
     <>
-      <div className="relative border p-2 rounded-md">
+      <div className="relative border p-2 rounded-md mb-2">
         <button onClick={() => removeComment(comment._id)} className="dark:text-slate-100 absolute top-0 right-[10px] text-[30px]">x</button>
         <h1 className="dark:text-slate-100 text-2xl">{comment.name}</h1>
         <p className="dark:text-slate-100 text-gray-700">{comment.content}</p>
