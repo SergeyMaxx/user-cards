45ed36a44ef48922f2d9012a20af6ef6b60a7406
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 11:28:45 +0200

    badge is done

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,29 +1,17 @@
 import React from 'react'
-// import './badge.css'
+import './badge.css'
 
 const Badge = ({ color, text }) => {
-  // const myFunction = () => {
-  //   badgeElement.animate(
-  //     [
-  //       // keyframes
-  //       { boxShadow: `0px 0px 10px 0px ${color}` },
-  //       { boxShadow: `0px 0px 0px 0px ${color}` },
-  //       { boxShadow: `0px 0px 10px 0px ${color}` }
-  //     ],
-  //     {
-  //       // timing options
-  //       duration: 1000,
-  //       iterations: Infinity,
-  //     }
-  //   )
-  // }
-
   return (
     <div
-      style={{boxShadow: `0px 0px 10px 0px ${color}`}}
-      className='py-[7px] px-[15px] m-[7px] rounded-lg'
+      style={{ boxShadow: `0px 0px 0px 0px ${color}`, color: color }}
+      className='badge py-[7px] px-[15px] m-[7px] rounded-lg'
     >
-      {text}
+      <div
+        style={{ boxShadow: '0px 0px 10px 0px inherit' }}
+      >
+        {text}
+      </div>
     </div>
   )
 }
