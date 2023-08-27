3930ade769660ea7d84e30d1a6722ef28db927b9
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 17:03:34 +0200

    some fix

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const appRoutes = useRoutes(routes);
   return (
-    <div className="dark:bg-slate-800 flex justify-center relative min-h-screen px-1 ">
-      <div className="flex flex-col items-center w-full max-w-7xl py-4">
+    <div className="dark:bg-slate-800 flex justify-center relative min-h-screen px-1">
+      <div className="flex flex-col items-center w-full max-w-[1200px] py-4">
         <header className="  container ">
           <NavBar />
         </header>
-        <main className="h-full  w-full container"><Breadcrumbs />{appRoutes}</main>
+        <main className="h-full  w-full container mb-[30px]"><Breadcrumbs />{appRoutes}</main>
         <footer className="items-center relative  rounded-t-2xl container">
           <span className=" px-2 text-xl text-gray-500">&copy; <Link target="_blank" to={"https://result.school"} className="hover:dark:text-slate-100 hover:text-gray-800 transition duration-300">Result School</Link> Team 102</span>
         </footer>
