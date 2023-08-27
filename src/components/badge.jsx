ec33f2cd43950503781b055cfd6929d94b333ce0
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 11:34:25 +0200

    corrected Badge

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -3,12 +3,10 @@ import './badge.css'
 
 const Badge = ({ color, text }) => {
   return (
-    <div
-      style={{ boxShadow: `0px 0px 0px 0px ${color}`, color: color }}
-      className='badge py-[7px] px-[15px] m-[7px] rounded-lg'
-    >
+    <div style={{ color: color }}>
       <div
         style={{ boxShadow: '0px 0px 10px 0px inherit' }}
+        className='badge py-[7px] px-[15px] m-[7px] rounded-lg'
       >
         {text}
       </div>
