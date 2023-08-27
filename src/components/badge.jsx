ad9583dcc3ea8efc0c8ac96b834b64b78bee6a70
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 11:51:35 +0200

    setColorState

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './badge.css'
 
 const Badge = ({ color, text }) => {
+  const [colorState, setColorState] = useState()
+
+  if (!colorState) {
+    setColorState(color)
+  }
+
   return (
-    <div style={{ color: color }}>
+    <div style={{ color: colorState }}>
       <div
         style={{ boxShadow: '0px 0px 10px 0px inherit' }}
         className='badge py-[7px] px-[15px] m-[7px] rounded-lg'
