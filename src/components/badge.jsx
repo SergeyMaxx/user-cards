c1d0581ff651add8aeafd254671124b1800a3f01
ZagSerSan <yan.steys1997@gmail.com>
2023-08-27 12:03:22 +0200

    correct 'badge' and 'students'

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './badge.css'
 
 const Badge = ({ color, text }) => {
@@ -9,7 +9,7 @@ const Badge = ({ color, text }) => {
   }
 
   return (
-    <div style={{ color: color }}>
+    <div style={{ color: colorState }}>
       <div
         style={{ boxShadow: '0px 0px 10px 0px inherit' }}
         className='badge py-[7px] px-[15px] m-[7px] rounded-lg'
