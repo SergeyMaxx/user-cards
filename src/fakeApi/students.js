f35b797d11ac5cff20bb31f8bfe2edb72a4208f9
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2023-08-27 15:49:01 +0300

    добавил свои данные к апи

--- src/fakeApi/students.js
+++ src/fakeApi/students.js
@@ -1,102 +1,105 @@
 const students = [
-   {
-      id: '1',
-      firstName: 'Андрей',
-      lastName: 'Родин',
-      age: 35,
-      info: 'Год назад я решил изменить свою жизнь и начал изучать JS',
-      infoBefore: 'Раньше работал на гигантском грузовике с прицепом',
-      img: 'https://i.postimg.cc/x1NvgTd5/itsme.jpg',
-      social: [{ type: "tg", to: "" }, { type: "vk", to: "https://vk.com" }],
-      projectRole: 'В этом проекте я делал структуру, hoc, роуты, навигацию, хлебные крошки, страницу студента, прогресс-бары и слайдер угнал тоже я)',
-      role: 'студент',
-      skills: [{ label: 'HTML', value: 70 }, { label: 'TypeScript', value: 80 }, { label: 'Css', value: 60 }, { label: 'JS', value: 90 }],
-      hobbies: ["Футбол", "Хардкор", "Кино", "Еда"],
-      portfolio: ['https://i.postimg.cc/8C9FPdpR/2023-08-26-14-24-47.png', 'https://i.postimg.cc/wB88q7Kt/2023-08-26-14-26-13.png']
+  {
+    id: '1',
+    firstName: 'Андрей',
+    lastName: 'Родин',
+    age: 35,
+    info: 'Год назад я решил изменить свою жизнь и начал изучать JS',
+    infoBefore: 'Раньше работал на гигантском грузовике с прицепом',
+    img: 'https://i.postimg.cc/x1NvgTd5/itsme.jpg',
+    social: [{ type: 'tg', to: '' }, { type: 'vk', to: 'https://vk.com' }],
+    projectRole: 'В этом проекте я делал структуру, hoc, роуты, навигацию, хлебные крошки, страницу студента, прогресс-бары и слайдер угнал тоже я)',
+    role: 'студент',
+    skills: [{ label: 'HTML', value: 70 }, { label: 'TypeScript', value: 80 }, {
+      label: 'Css',
+      value: 60
+    }, { label: 'JS', value: 90 }],
+    hobbies: ['Футбол', 'Хардкор', 'Кино', 'Еда'],
+    portfolio: ['https://i.postimg.cc/8C9FPdpR/2023-08-26-14-24-47.png', 'https://i.postimg.cc/wB88q7Kt/2023-08-26-14-26-13.png']
 
-   },
-   {
-      id: "2",
-      firstName: "Сергей",
-      lastName: "Загорский",
-      age: 25,
-      info: "Изучаю frontend",
-      infoBefore: "Ранее, во время универа, решил поменять профессию и начал развиваться в ИТ.",
-      img: "https://i.postimg.cc/jSN66fh5/500-500.jpg",
-      social: [{ type: "tg", to: "https://@ZagSerSan" }, { type: "vk", to: "https://vk.com" }],
-      projectRole: "отдыхал :D",
-      role: "студент",
-      skills: [
-        { label: "HTML", value: 50 },
-        { label: "Css", value: 70 },
-        { label: "JavaScript", value: 60 },
-        { label: "React", value: 30 }
-      ],
-      hobbies: ['Гитара', 'Документальные фильмы', 'Общение', 'Бизнес', 'Музыка'],
-      portfolio: ["https://i.postimg.cc/Kjw8cJ4D/weather-widget.png", "https://i.postimg.cc/KckKwM1D/tic-tac-toe.png"]
-   },
-   {
-      id: '3',
-      firstName: 'Сергей',
-      lastName: 'Максимов',
-      age: 39,
-      info: 'Frontend разработчик',
-      infoBefore: 'учился в Result School',
-      img: 'https://i.postimg.cc/j5H8gPzQ/Smart-Select-Image-2020-01-14-20-48-50-1.png',
-      social: [{ type: "tg", to: "" }, { type: "vk", to: "https://vk.com" }],
-      projectRole: 'MainPage, Button',
-      role: 'Team Lead',
-      skills: [
-         { label: 'HTML', value: 50 },
-         { label: 'JavaScript', value: 80 },
-         { label: 'React', value: 50 },
-         { label: 'Redux', value: 50 },
-         { label: 'CSS', value: 30 },
-      ],
-      hobbies: [],
-      portfolio: [
-         'https://i.postimg.cc/vTyHLZD4/fast-company.png',
-         'https://i.postimg.cc/768xwZ3K/notepad.png',
-         'https://i.postimg.cc/wxk6YH7k/todos.png'
-      ]
-   }]
+  },
+  {
+    id: '2',
+    firstName: 'Сергей',
+    lastName: 'Загорский',
+    age: 25,
+    info: 'Изучаю frontend',
+    infoBefore: 'Ранее, во время универа, решил поменять профессию и начал развиваться в ИТ.',
+    img: 'https://i.postimg.cc/jSN66fh5/500-500.jpg',
+    social: [{ type: 'tg', to: 'https://@ZagSerSan' }, { type: 'vk', to: 'https://vk.com' }],
+    projectRole: 'отдыхал :D',
+    role: 'студент',
+    skills: [
+      { label: 'HTML', value: 50 },
+      { label: 'Css', value: 70 },
+      { label: 'JavaScript', value: 60 },
+      { label: 'React', value: 30 }
+    ],
+    hobbies: ['Гитара', 'Документальные фильмы', 'Общение', 'Бизнес', 'Музыка'],
+    portfolio: ['https://i.postimg.cc/Kjw8cJ4D/weather-widget.png', 'https://i.postimg.cc/KckKwM1D/tic-tac-toe.png']
+  },
+  {
+    id: '3',
+    firstName: 'Сергей',
+    lastName: 'Максимов',
+    age: 39,
+    info: 'Frontend разработчик',
+    infoBefore: 'работал много, получал мало и решил это исправить',
+    img: 'https://i.postimg.cc/j5H8gPzQ/Smart-Select-Image-2020-01-14-20-48-50-1.png',
+    social: [{ type: 'tg', to: '' }, { type: 'vk', to: 'https://vk.com' }],
+    projectRole: 'В этом проекте я делал главную страницу, компонент кнопки, и так по мелочи',
+    role: 'Team Lead',
+    skills: [
+      { label: 'HTML', value: 50 },
+      { label: 'JavaScript', value: 80 },
+      { label: 'React', value: 50 },
+      { label: 'Redux', value: 50 },
+      { label: 'CSS', value: 30 },
+    ],
+    hobbies: ['кино', 'музыка', 'гитара', 'спорт'],
+    portfolio: [
+      'https://i.postimg.cc/vTyHLZD4/fast-company.png',
+      'https://i.postimg.cc/768xwZ3K/notepad.png',
+      'https://i.postimg.cc/wxk6YH7k/todos.png'
+    ]
+  }];
 
 
 const fetchAllStudents = () =>
-   new Promise((resolve) => {
-      setTimeout(() => {
-         resolve(students);
-      }, 1000);
-   });
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(students);
+    }, 1000);
+  });
 
 export const toggleFavorite = (id) => {
-   if (localStorage.getItem('favorite')) {
-      let favoriteState = JSON.parse(localStorage.getItem('favorite'))
+  if (localStorage.getItem('favorite')) {
+    let favoriteState = JSON.parse(localStorage.getItem('favorite'));
 
-      const isContain = favoriteState.includes(id)
+    const isContain = favoriteState.includes(id);
 
-      isContain
-         ? favoriteState = favoriteState.filter(thisId => thisId !== id)
-         : favoriteState.push(id)
+    isContain
+      ? favoriteState = favoriteState.filter(thisId => thisId !== id)
+      : favoriteState.push(id);
 
-      favoriteState.length === 0
-         ? localStorage.removeItem('favorite')
-         : localStorage.setItem('favorite', JSON.stringify(favoriteState));
+    favoriteState.length === 0
+      ? localStorage.removeItem('favorite')
+      : localStorage.setItem('favorite', JSON.stringify(favoriteState));
 
-      return favoriteState
-   } else {
-      localStorage.setItem("favorite", JSON.stringify([id]))
-      return JSON.parse(localStorage.getItem('favorite'))
-   }
-}
+    return favoriteState;
+  } else {
+    localStorage.setItem('favorite', JSON.stringify([id]));
+    return JSON.parse(localStorage.getItem('favorite'));
+  }
+};
 
 export const getBookmarkStatus = (id) => {
-   if (localStorage.getItem('favorite')) {
-      let favoriteState = JSON.parse(localStorage.getItem('favorite'))
-      const isContain = favoriteState.includes(id)
-      return isContain
-   }
-   return false
-}
+  if (localStorage.getItem('favorite')) {
+    let favoriteState = JSON.parse(localStorage.getItem('favorite'));
+    const isContain = favoriteState.includes(id);
+    return isContain;
+  }
+  return false;
+};
 
 export default fetchAllStudents;
